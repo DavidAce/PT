05cb3f295d4de7baf0695d4904cc6251e960d521
DavidAce <kameboy@gmail.com>
2016-11-30 23:20:51 +0100

    Modify input class

--- bash_scripts/createInput/createInput.py
+++ bash_scripts/createInput/createInput.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 import os
 import sys
-import fileinput
-import decimal
+import itertools
 
-textToReplace = ["T_min=","T_max=", "J1=", "J2="]
-fileToSearch  = "../../input/default/default.dat"
+model = sys.argv[1:] if sys.argv[1:] else 'default'
+print (model)
+outputPath = "../../input/" + model + "/"
+fileToSearch  = "../../input/template.dat"
+textToReplace = ["T_min=","T_max=", "J1=", "J2=", "L=", "MCS_warmup", "MCS_sample"]
+T_min_file  =  open('T_min.dat', 'r').read().splitlines()
+T_max_file  =  open('T_max.dat', 'r').read().splitlines()
+J1_file     =  open('J1.dat', 'r').read().splitlines()
+J2_file     =  open('J2.dat', 'r').read().splitlines()
+L_file      =  open('L.dat', 'r').read().splitlines()
+MCS_warmup_file  =  open('MCS_warmup.dat', 'r').read().splitlines()
+MCS_sample_file  =  open('MCS_sample.dat', 'r').read().splitlines()
 
+for (T_min,T_max, J1, J2,L, MCS_warmup, MCS_sample) in itertools.product(T_min_file, T_max_file, J1_file, J2_file, L_file, MCS_warmup_file, MCS_sample_file):
+        fileToWrite = outputPath + model + '_L[' + L + ']_J1[' + J1 + ']_J2['  + J2 +  '].dat'
+        os.makedirs(os.path.dirname(fileToWrite), exist_ok=True)
 
-with open(fileToSearch, 'r') as infile, open('dummy_new', 'w') as outfile:
-    for line in infile:
-        if len(line.split()) < 1:
-            continue;
-        for iteration, (i,j) in enumerate(zip(line.split(),textToReplace)):
-            print(i,j)
-
-
-            if(i == j):
-                print(line.split()[1])
-
-        #if (len(line.split()) < 2): continue;
-        #print (any(substring in line for substring in textToReplace))
-        #print (any(textToReplace == word for word in line.split()))
-        #print (line.index(substring) for substring in textToReplace)
-
-        #if (any(substring in line for substring in textToReplace)):
-
-        #for text in textToReplace:
-            #print( line.replace(text + line.split()[1], text + "test"))
-            #outfile.write(line.replace(text + line.split()[1], text + "test"))
+        with open(fileToSearch, 'r') as infile, open(fileToWrite, 'w') as outfile:
+            T_min_adj = T_min if (float(J1) == 0) else str(float(T_min) / float(J1))
+            T_max_adj = T_max if (float(J1) == 0) else str(float(T_max) / float(J1))
+            for line in infile:
+                line_array  = line.split();
+                newline = line;
+                if len(line_array) > 1:
+                    newline  = newline.replace(textToReplace[0] + " " + line_array[1], textToReplace[0] + " " + T_min_adj)
+                    newline  = newline.replace(textToReplace[1] + " " + line_array[1], textToReplace[1] + " " + T_max_adj)
+                    newline  = newline.replace(textToReplace[2] + " " + line_array[1], textToReplace[2] + " " + J1)
+                    newline  = newline.replace(textToReplace[3] + " " + line_array[1], textToReplace[3] + " " + J2)
+                    newline  = newline.replace(textToReplace[4] + " " + line_array[1], textToReplace[4] + " " + L)
+                    newline  = newline.replace(textToReplace[5] + " " + line_array[1], textToReplace[5] + " " + MCS_warmup)
+                    newline  = newline.replace(textToReplace[6] + " " + line_array[1], textToReplace[6] + " " + MCS_sample)
+                outfile.write(newline)
