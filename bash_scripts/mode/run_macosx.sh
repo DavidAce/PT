36f311760d3ee61c44561ea3e3eda804a9222915
DavidAce <kameboy@gmail.com>
2018-02-02 15:24:04 +0100

    fixing macos compatibility

---
+++ bash_scripts/mode/run_macosx.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+function is_int() { return $(test "$@" -eq "$@" > /dev/null 2>&1); }
+ARG1=${1:-"Release"}
+ARG2=${2:-"default"}
+for var in "$@"
+do
+    if $(is_int "$var");
+    then
+        echo "Number of cores: $var"
+        NP=$var
+    fi
+done
+NP=${12-NP}
+NUMARGS=$#
+echo "Arguments ($NUMARGS): $@"
+
+
+if [[ NUMARGS -le 1 ]]
+then
+    if [[ $ARG1 == *"valgrind"* ]]
+    then
+        echo "Running PT with option valgrind"
+	mpirun --mca mpi_show_handle_leaks 1 -n 4 valgrind --tool=memcheck --leak-check=full ./build/Debug/PT
+        #valgrind --tool=memcheck --leak-check=full -v mpirun -n 4 --mca mpi_show_handle_leaks 1 ./build/Debug/PT
+    elif [[ $ARG1 == *"gdb"* ]]
+    then
+        echo "Running PT with option gdb"
+        mpirun -n 4 xterm -e gdb ./build/Debug/PT
+
+    elif [[ $ARG1 == *"ebug"* ]]
+    then
+        ulimit -c unlimited
+        echo "Running PT in Debug-mode"
+        mpirun -n 12 ./build/Debug/PT
+    elif [[ $ARG1 == *"elease"* ]]
+    then
+        echo "Running PT in Release-mode"
+        ulimit -c unlimited
+        mpirun -n 12  ./build/Release/PT
+    elif [[ $ARG1 == *"atch" ]]
+    then
+        echo "Use syntax: ./run.sh batch [jobname]"
+    else
+        echo "Use syntax: ./run.sh batch [jobname]"
+    fi
+elif [[ NUMARGS -gt 1 ]]
+then
+    if [[ $ARG1 == *"atch"* ]]
+    then
+        echo "Running PT in Batch-mode"
+        ulimit -c unlimited
+        for filename in input/$ARG2/*.dat; do
+            echo "Executing file: $filename"
+            mpirun -n 12  ./build/Release/PT $ARG2 "$filename"
+        done
+    else
+        echo "Use syntax: ./run.sh batch [jobname]"
+    fi
+fi
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+}
+
+
+
--- build.sh
+++ build.sh
@@ -28,6 +28,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     echo "OS: Mac OSX"
+    echo "Checking that GCC is installed in homebrew: [brew ls gcc | grep -q 'g++-7']"
     if brew ls gcc | grep -q 'g++-7'; then
         echo "GCC-7 is installed"
         export CC=gcc-7
