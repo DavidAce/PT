5dff7efa1fb5f76119d0f2d8b9192ac6a261cf1b
DavidAce <kameboy@gmail.com>
2018-02-02 15:18:44 +0100

    fixing macos compatibility

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 #rm -rf *
 buildtype="Release"
-
+dcmake_c_compiler=""
+dcmake_cxx_compiler=""
 if [[ "$@" == *"ebug"* ]]
 then
 	buildtype="Debug"
@@ -12,28 +13,42 @@ if [[ "$@" == *"lean"* ]]
 then
     echo "Cleaning build"
 	rm -rf build
+	rm -rf cmake/download_scripts/tmp/
     exit 0
 fi
 
-
-if [[ "$HOSTNAME" == *"triolith"* ]]
-then
-    echo "We're on triolith!";
-    module add cmake/3.6.1
-    module load buildenv-intel/2016-3
-    export CC=/software/apps/gcc/5.3.0/build01/bin/gcc
-    export CXX=/software/apps/gcc/5.3.0/build01/bin/g++
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "OS: Linux"
+    if [[ "$HOSTNAME" == *"triolith"* ]]; then
+        echo "We're on triolith!";
+        module add cmake/3.6.1
+        module load buildenv-intel/2016-3
+        export CC=/software/apps/gcc/5.3.0/build01/bin/gcc
+        export CXX=/software/apps/gcc/5.3.0/build01/bin/g++
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "OS: Mac OSX"
+    if brew ls gcc | grep -q 'g++-7'; then
+        echo "GCC-7 is installed"
+        export CC=gcc-7
+        export CXX=g++-7
+        dcmake_c_compiler="-DCMAKE_C_COMPILER=gcc-7"
+        dcmake_cxx_compiler="-DCMAKE_CXX_COMPILER=g++-7"
+    else
+        echo "Please install GCC (version 7) through homebrew"
+    fi
 else
-    echo "We're on my pc!"
+        echo "Could not identify OS"
 fi
+
+
 rm -rf build
 mkdir build
 cd build
 mkdir ${buildtype}
 cd ${buildtype}
-export CC=gcc-7
-export CXX=g++-7
+
 
 echo "Starting Build"
-cmake -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DCMAKE_BUILD_TYPE=${buildtype}  ../../
+cmake ${dcmake_c_compiler} ${dcmake_cxx_compiler} -DCMAKE_BUILD_TYPE=${buildtype}  ../../
 make
--- run.sh
+++ run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 option=$@
 echo "Starting job"
-if [[ "$HOSTNAME" == *"triolith"* ]]
-then
-    echo "We're on triolith!";
-    if [[ "${option}" == *"valgrind"* ]]
-    then
-        sbatch bash_scripts/mode/run_triolith_debug.sh
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    echo "OS: Linux"
+    if [[ "$HOSTNAME" == *"triolith"* ]]; then
+        echo "We're on triolith!";
+        if [[ "${option}" == *"valgrind"* ]]; then
+            sbatch bash_scripts/mode/run_triolith_debug.sh
+        else
+            sbatch bash_scripts/mode/run_triolith.sh
+        fi
     else
-        sbatch bash_scripts/mode/run_triolith.sh
+        bash_scripts/mode/run_linux.sh ${option}
     fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "OS: Mac OSX"
+    bash_scripts/mode/run_linux.sh ${option}
 else
-    echo "We're on my pc!"
-    ./bash_scripts/mode/run_my_pc.sh ${option}
+        echo "Could not identify OS"
 fi
